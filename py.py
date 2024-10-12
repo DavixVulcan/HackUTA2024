b4635b495a60d60f50bff446f4e200da5740c61c
Juan Tavera <askywardttv@gmail.com>
2024-10-12 17:10:45 -0500

    Update 1

--- py.py
+++ py.py
@@ -1,1 +1,13 @@
-from flask import Flask, 
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "Hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+@app.route('/')
+def home():
+    return render_template('HTML.html')
+if __name__ == "__main__":
+    app.run(debug=True)
