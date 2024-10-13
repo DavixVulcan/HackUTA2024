9dfc0884f5b3a9f259336a4d585b98bad0685da6
DavixVulcan <Davix1648@gmail.com>
2024-10-13 01:08:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,21 +13,21 @@ def heartrate_mon():
         if device.get_heartrate() > 100:
             device.alert()
             socketio.emit('alert', {'datatype': 'heartrate', 'data':str(device.get_heartrate())}, broadcast=True)
-            time.sleep(1)
+        time.sleep(1)
 
 def bloodpressure_mon():
     while True:
         if device.get_bloodpressure() > 120:
             device.alert()
             socketio.emit('alert', {'datatype': 'bloodpressure', 'data':str(device.get_bloodpressure())}, broadcast=True)
-            time.sleep(1)
+        time.sleep(1)
 
 def bloodoxy_mon():
     while True:
         if device.get_bloodoxygen() < 90:
             device.alert()
             socketio.emit('alert', {'datatype': 'bloodoxy', 'data':str(device.get_bloodoxygen())}, broadcast=True)
-            time.sleep(1)
+        time.sleep(1)
 
 @app.route('/')
 def home():
@@ -46,7 +46,7 @@ def main_looper():
     thread2.start()
     thread3 = Thread(target=bloodoxy_mon)
     thread3.start()
-    
+
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
 
