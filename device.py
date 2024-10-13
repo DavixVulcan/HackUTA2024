5f9a20e7aa10748c2fe00e362e0de63493b01c1c
DavixVulcan <Davix1648@gmail.com>
2024-10-12 21:20:02 -0500

    Update device.py

--- device.py
+++ device.py
@@ -9,19 +9,37 @@ button = Button(27)
 
 position = 0
 
+bloodtrue = True
+heartrate = 100
+bloodpressure = 100
+
 # Function to handle the rotary encoder's movement
 def rotary_moved():
     global position
+    global bloodpressure
+    global heartrate
+    additive = 0
     if encoder.steps < position:
         print("Rotated Right (Clockwise)")
+        additive = 1
     else:
         print("Rotated Left (Counterclockwise)")
+        additive = -1
     position = encoder.steps
     print(f"Position: {encoder.steps}")
+
+    if bloodtrue:
+        bloodpressure += additive
+    else:
+        heartrate += additive
+
+    print(f"bloodpressure: {bloodpressure}")
+    print(f"heartrate: {heartrate}")
 
 # Function to handle button press
 def button_pressed():
     print("Button pressed!")
+    bloodtrue = not bloodtrue
 
 # Assign event handlers for movement and button press
 encoder.when_rotated = rotary_moved
